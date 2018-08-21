7b7ecd6f321547a25f6a34b5bc2fb4de32a72137
Anshul Mittal <anshulmittal0409@gmail.com>
2018-08-21 13:40:56 +0530

    Add temporary tests to ratePopu.py
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- geneticAlgo/genetic_algo_ratePopu.py
+++ geneticAlgo/genetic_algo_ratePopu.py
@@ -8,4 +8,17 @@ def rankDishes(population):
     fitnessResults = {}
     for i in population:
         fitnessResults[i] = Fitness(population[i]).calcFitness()
-    return sorted(fitnessResults, key=lambda x: x[1], reverse=True)
+    return sorted(fitnessResults.items(), key=lambda x: x[1], reverse=True)
+
+# Testing sorted()
+# import json
+
+# with open('../dishMap.json') as dish_map:
+#     dishMap = json.load(dish_map)
+
+# tempData = {}
+
+# for i in dishMap:
+#     tempData[dishMap[i]["dishName"]] = int(i)
+
+# print (sorted(tempData.items(),key = lambda x:x[1], reverse= True))
