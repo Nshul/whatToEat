6fcc5c3840857536dc042b6cc7c6540391af0cfb
Anshul Mittal <anshulmittal0409@gmail.com>
2018-08-21 14:36:17 +0530

    Modify the population rating algorithm
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- geneticAlgo/genetic_algo_ratePopu.py
+++ geneticAlgo/genetic_algo_ratePopu.py
@@ -1,24 +1,22 @@
 from genetic_algo_fitness import Fitness
+from genetic_algo_initial_popu import createInitialPopu
 
-def rankDishes(population):
+def rankDishes(population, cuisineScore):
     """
     Ranks the population using the Fitness class
     and the method to calculate fitness
     """
     fitnessResults = {}
-    for i in population:
-        fitnessResults[i] = Fitness(population[i]).calcFitness()
+    for i in range(0,len(population)):
+        fitnessResults[i] = Fitness(population[i], cuisineScore).calcFitness()
     return sorted(fitnessResults.items(), key=lambda x: x[1], reverse=True)
 
-# Testing sorted()
-# import json
-
-# with open('../dishMap.json') as dish_map:
-#     dishMap = json.load(dish_map)
-
-# tempData = {}
-
-# for i in dishMap:
-#     tempData[dishMap[i]["dishName"]] = int(i)
-
-# print (sorted(tempData.items(),key = lambda x:x[1], reverse= True))
+# temp = createInitialPopu(4, 10)
+# print ("***")
+# print (temp)
+# print ("***")
+# cuisineScore = {"indian":0.33,"italian":0.45,"afghani":0.08,"chinese":0.14}
+# temp1 = rankDishes(temp, cuisineScore)
+# print ("/*/*")
+# print (temp1)
+# print ("/*/*")
