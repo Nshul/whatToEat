dbfaa80f56f2457a48931c0a99e1df3645d410a9
Anshul Mittal <anshulmittal0409@gmail.com>
2018-10-05 23:53:18 +0530

    Add testGA.py so as to test the GA
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

---
+++ geneticAlgo/testGA.py
@@ -0,0 +1,40 @@
+from genetic_algo_initial_popu import createInitialPopu
+from genetic_algo_ratePopu import rankDishes
+from selection import selection
+from crossover import crossover
+
+initialPopu = open('./tempFiles/initPopu.txt','w')
+PopuWithRank = open('./tempFiles/popuWithFit.txt','w')
+matingPoolAfterFirstSelection = open('./tempFiles/afterFirstSelection.txt','w')
+popuNewGen = open("./tempFiles/populationAfterCrossover.txt",'w')
+
+cuisineScore = {"indian":0.33,"italian":0.45,"afghani":0.08,"chinese":0.14}
+
+temp = createInitialPopu(4, 10)
+for item in temp:
+    initialPopu.write("%s\n" %item)
+temp1 = rankDishes(temp, cuisineScore)
+for item in temp1:
+    PopuWithRank.write("%s\n" %str(item))
+
+matePool = selection(temp1,2)
+for item in matePool:
+    matingPoolAfterFirstSelection.write("%s\n" %str(item))
+
+newGen = crossover(matePool,temp,5)
+for item in newGen:
+    popuNewGen.write("%s\n" %str(item))
+
+# p1 = indian, italian, chinese
+# p2 = italian, indian, chinese
+# p3 = italian, indian, chinese
+# p4 = italian, afghani, indian
+
+# indian = 8+2*5+3=21
+# italian = 8*3+5 = 29
+# afghani = 5
+# chinese = 3*3 = 9
+
+# total = (8+5+3)*4 = 64
+
+
