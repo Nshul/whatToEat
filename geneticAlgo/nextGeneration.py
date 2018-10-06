ba5922b00475660055ad641fadf415f870610fb5
Anshul Mittal <anshulmittal0409@gmail.com>
2018-10-06 13:30:28 +0530

    Add description of nextGeneration function
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- geneticAlgo/nextGeneration.py
+++ geneticAlgo/nextGeneration.py
@@ -3,10 +3,22 @@ from selection import selection
 from crossover import crossover
 from mutate import mutatePopulation
 
-def nextGeneration ( currentPopulation, cuisineScore, noOfElite, mutationRate ):
-    populationRanked = rankDishes(currentPopulation, cuisineScore)
-    selectedPopulationPool = selection( populationRanked, noOfElite)
-    populationAfterCrossover = crossover( selectedPopulationPool, currentPopulation, noOfElite)
-    nextGen = mutatePopulation( populationAfterCrossover, mutationRate )
-    return nextGen
 
+def nextGeneration(currentPopulation, cuisineScore, noOfElite, mutationRate):
+    """
+    This function applies genetic operators over current generation to produce 
+    new generation
+
+    currentPopulation -> current pool of chromosomes of current generation
+    cuisineScore -> Object consisting of cuisine score as rated by the users
+    noOfElite -> no of Elite chromosomes that are to be persisted in next generation
+    mutationRate -> rate at which mutation is to take place
+
+    returns new generation of chromosomes
+    """
+    populationRanked = rankDishes(currentPopulation, cuisineScore)
+    selectedPopulationPool = selection(populationRanked, noOfElite)
+    populationAfterCrossover = crossover(
+        selectedPopulationPool, currentPopulation, noOfElite)
+    nextGen = mutatePopulation(populationAfterCrossover, mutationRate)
+    return nextGen
